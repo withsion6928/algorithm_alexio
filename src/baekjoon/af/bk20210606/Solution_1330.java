bdb5e160b268363c3ac0c622be4e7414f87fa4c2
withsion6928 <withsion6928@gmail.com>
2021-06-07 23:04:22 +0900

    20210607 두 수 비교하기

---
+++ src/baekjoon/af/bk20210606/Solution_1330.java
@@ -0,0 +1,18 @@
+package baekjoon.af.bk20210606;
+
+import java.util.Scanner;
+
+public class Solution_1330 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		String result  = a < b ? "<" : a > b ? ">" : "==";
+
+		System.out.println(result);
+
+	}
+
+
+}
