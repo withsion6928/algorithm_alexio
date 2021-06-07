70916abb0590419aeca161fb1fee13f96d9c2834
withsion6928 <withsion6928@gmail.com>
2021-06-07 23:16:34 +0900

    20210607 두 수 비교하기

--- src/baekjoon/af/bk20210606/Solution_1330.java
+++ src/baekjoon/af/bk20210606/Solution_1330.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Solution_1330 {
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
 		int b = sc.nextInt();
